878b921091b918fc1bedf370b84e76213df9b058
ashfaqur-rashidmo <ashfaqurrashid1@gmail.com>
2026-02-13 11:09:56 +0600

    update frontend: fix APIs and remove sanity

--- components/ProductCharacteristics.jsx
+++ components/ProductCharacteristics.jsx
@@ -1,25 +1,3 @@
-// import { getBrand } from '@/sanity/queries';
-// import React from 'react'
-// import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion';
-
-// const ProductCharacteristics = async({product}) => {
-//     const brand = await getBrand(product?.slug?.current);
-//     console.log(brand);
-    
-//   return (
-//     <Accordion type="single" collapsible>
-//      <AccordionItem value="item-1">
-//       <AccordionTrigger>{product?.name}: Characteristics</AccordionTrigger>
-//       <AccordionContent>
-//         <p>Brand: {brand && <span>{brand[0]?. brandName}</span>}</p>
-//       </AccordionContent>
-//      </AccordionItem>
-//     </Accordion>
-//   )
-// }
-
-// export default ProductCharacteristics
-
 "use client";
 
 import {
